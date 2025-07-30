2fe9fa1e2445a135e1a89cb5977faec4c4bbc96a
Elena Parapounsky <eparapounsky@gmail.com>
2025-07-30 10:29:03 -0700

    format with Black Formatter

--- ChessVar.py
+++ ChessVar.py
@@ -17,6 +17,7 @@ WP = 60  # white pawn
 
 class Piece(Enum):
     """Enumeration for chess pieces"""
+
     __ = __  # empty space
     BR = BR  # black rook
     BH = BH  # black horse/knight
@@ -49,7 +50,7 @@ class ChessVar:
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [60, 60, 60, 60, 60, 60, 60, 60],
-            [10, 20, 30, 40, 50, 30, 20, 10]
+            [10, 20, 30, 40, 50, 30, 20, 10],
         ]
         self._game_state = "UNFINISHED"
         self._current_player = "WHITE"
@@ -85,58 +86,84 @@ class ChessVar:
         # player cannot blow up both kings at once
         if self._board[destination_row][destination_column] != 0:
             pieces_list = []
-            positions = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+            positions = [
+                (0, 0),
+                (1, 0),
+                (-1, 0),
+                (0, 1),
+                (0, -1),
+                (1, 1),
+                (1, -1),
+                (-1, 1),
+                (-1, -1),
+            ]
             for row, column in positions:
                 try:
-                    pieces_list.append(self._board[destination_row + row][destination_column + column])
+                    pieces_list.append(
+                        self._board[destination_row + row][destination_column + column]
+                    )
                 except IndexError:  # explosion on board edge
                     continue
             if 5 in pieces_list and 50 in pieces_list:
                 return False
 
-    def check_horizontal_vertical_move(self, current_column, current_row, destination_column, destination_row):
+    def check_horizontal_vertical_move(
+        self, current_column, current_row, destination_column, destination_row
+    ):
         """Checks if there are any pieces in the way of a potential horizontal or vertical move.
         :param current_column: int, column of current piece location
         :param current_row: int, row of current piece location
         :param destination_column: int, column of potential new piece location
         :param destination_row: int, row of potential new piece location"""
 
-        if current_row == destination_row and current_column < destination_column:  # horizontal, to right
+        if (
+            current_row == destination_row and current_column < destination_column
+        ):  # horizontal, to right
             checked_column = current_column + 1
             while checked_column != destination_column:
                 if self._board[current_row][checked_column] != 0:
                     return False
                 checked_column += 1
 
-        elif current_row == destination_row and current_column > destination_column:  # horizontal, to left
+        elif (
+            current_row == destination_row and current_column > destination_column
+        ):  # horizontal, to left
             checked_column = current_column - 1
             while checked_column != destination_column:
                 if self._board[current_row][checked_column] != 0:
                     return False
                 checked_column -= 1
 
-        elif current_column == destination_column and current_row > destination_row:  # vertical, to top
+        elif (
+            current_column == destination_column and current_row > destination_row
+        ):  # vertical, to top
             checked_row = current_row - 1
             while checked_row != destination_row:
                 if self._board[checked_row][current_column] != 0:
                     return False
                 checked_row -= 1
 
-        elif current_column == destination_column and current_row < destination_row:  # vertical, to bottom
+        elif (
+            current_column == destination_column and current_row < destination_row
+        ):  # vertical, to bottom
             checked_row = current_row + 1
             while checked_row != destination_row:
                 if self._board[checked_row][current_column] != 0:
                     return False
                 checked_row += 1
 
-    def check_diagonal_move(self, current_column, current_row, destination_column, destination_row):
+    def check_diagonal_move(
+        self, current_column, current_row, destination_column, destination_row
+    ):
         """Checks if there are any pieces in the way of a potential diagonal move.
         :param current_column: int, column of current piece location
         :param current_row: int, row of current piece location
         :param destination_column: int, column of potential new piece location
         :param destination_row: int, row of potential new piece location"""
 
-        if current_row > destination_row and current_column < destination_column:  # bottom to top, to right
+        if (
+            current_row > destination_row and current_column < destination_column
+        ):  # bottom to top, to right
             checked_row = current_row - 1
             checked_column = current_column + 1
             while checked_row != destination_row:
@@ -145,7 +172,9 @@ class ChessVar:
                 checked_row -= 1
                 checked_column += 1
 
-        elif current_row > destination_row and current_column > destination_column:  # bottom to top, to left
+        elif (
+            current_row > destination_row and current_column > destination_column
+        ):  # bottom to top, to left
             checked_row = current_row - 1
             checked_column = current_column - 1
             while checked_row != destination_row:
@@ -154,7 +183,9 @@ class ChessVar:
                 checked_row -= 1
                 checked_column -= 1
 
-        elif current_row < destination_row and current_column < destination_column:  # top to bottom, to right
+        elif (
+            current_row < destination_row and current_column < destination_column
+        ):  # top to bottom, to right
             checked_row = current_row + 1
             checked_column = current_column + 1
             while checked_row != destination_row:
@@ -163,7 +194,9 @@ class ChessVar:
                 checked_row += 1
                 checked_column += 1
 
-        elif current_row < destination_row and current_column > destination_column:  # top to bottom, to left
+        elif (
+            current_row < destination_row and current_column > destination_column
+        ):  # top to bottom, to left
             checked_row = current_row + 1
             checked_column = current_column - 1
             while checked_row != destination_row:
@@ -172,7 +205,9 @@ class ChessVar:
                 checked_row += 1
                 checked_column -= 1
 
-    def check_if_valid_chess_move(self, piece, current_column, current_row, destination_column, destination_row):
+    def check_if_valid_chess_move(
+        self, piece, current_column, current_row, destination_column, destination_row
+    ):
         """Checks if the move is allowed by regular chess rules.
         :param piece: int, represents the piece being moved
         :param current_column: int, column of current piece location
@@ -188,22 +223,37 @@ class ChessVar:
             return False
 
         # player cannot capture their own piece
-        if piece < 10 and 0 < self._board[destination_row][destination_column] < 10:  # black
+        if (
+            piece < 10 and 0 < self._board[destination_row][destination_column] < 10
+        ):  # black
             return False
-        if piece >= 10 and self._board[destination_row][destination_column] >= 10:  # white
+        if (
+            piece >= 10 and self._board[destination_row][destination_column] >= 10
+        ):  # white
             return False
 
         # rook can only move straight, no limit on spaces
-        if piece in (BR, WR) and current_row != destination_row and current_column != destination_column:
+        if (
+            piece in (BR, WR)
+            and current_row != destination_row
+            and current_column != destination_column
+        ):
             return False
         # check for pieces in rook's way
         if piece in (BR, WR):
-            if self.check_horizontal_vertical_move(current_column, current_row,
-                                                   destination_column, destination_row) is False:
+            if (
+                self.check_horizontal_vertical_move(
+                    current_column, current_row, destination_column, destination_row
+                )
+                is False
+            ):
                 return False
 
         # horse can only move in L shape, can jump over pieces
-        if piece in (BH, WH) and (row_distance, column_distance) not in [(1, 2), (2, 1)]:
+        if piece in (BH, WH) and (row_distance, column_distance) not in [
+            (1, 2),
+            (2, 1),
+        ]:
             return False
 
         # bishop can only move diagonally, no limit on spaces
@@ -211,24 +261,41 @@ class ChessVar:
             return False
         # check for pieces in bishop's way
         if piece in (BB, WB):
-            if self.check_diagonal_move(current_column, current_row, destination_column, destination_row) is False:
+            if (
+                self.check_diagonal_move(
+                    current_column, current_row, destination_column, destination_row
+                )
+                is False
+            ):
                 return False
 
         # queen can move in any direction, no limit on spaces
         if piece in (BQ, WQ):
-            if not (row_distance == column_distance or current_row == destination_row
-                    or current_column == destination_column):
+            if not (
+                row_distance == column_distance
+                or current_row == destination_row
+                or current_column == destination_column
+            ):
                 return False
 
             # check horizontal/vertical moves for blocking pieces
             if current_row == destination_row or current_column == destination_column:
-                if self.check_horizontal_vertical_move(current_column, current_row,
-                                                       destination_column, destination_row) is False:
+                if (
+                    self.check_horizontal_vertical_move(
+                        current_column, current_row, destination_column, destination_row
+                    )
+                    is False
+                ):
                     return False
 
             # check diagonal moves for blocking pieces
             if row_distance == column_distance:
-                if self.check_diagonal_move(current_column, current_row, destination_column, destination_row) is False:
+                if (
+                    self.check_diagonal_move(
+                        current_column, current_row, destination_column, destination_row
+                    )
+                    is False
+                ):
                     return False
 
         # king can only move 1 space at a time, any direction
@@ -242,20 +309,30 @@ class ChessVar:
             return False
 
         # pawns can only move diagonally if making a capture
-        if piece in (BP, WP) and self._board[destination_row][
-                                    destination_column] == 0 and current_column != destination_column:
+        if (
+            piece in (BP, WP)
+            and self._board[destination_row][destination_column] == 0
+            and current_column != destination_column
+        ):
             return False
 
         # pawns cannot make head-on captures (only diagonal)
-        if piece in (BP, WP) and self._board[destination_row][
-                                    destination_column] != 0 and current_column == destination_column:
+        if (
+            piece in (BP, WP)
+            and self._board[destination_row][destination_column] != 0
+            and current_column == destination_column
+        ):
             return False
 
         # pawns can move forward 2 spaces on first turn, 1 thereafter
         if piece in (BP, WP) and (piece in self._board[1] or piece in self._board[6]):
             if row_distance > 2:
                 return False
-        elif piece in (BP, WP) and piece not in self._board[1] and piece not in self._board[6]:
+        elif (
+            piece in (BP, WP)
+            and piece not in self._board[1]
+            and piece not in self._board[6]
+        ):
             if row_distance > 1:
                 return False
 
@@ -266,25 +343,36 @@ class ChessVar:
         :param square_moved_to: string, the desired destination position of a piece on the board
         :return: False-if move is invalid, True-if move is valid"""
 
-        current_position = list(square_moved_from)  # splits string into a list of characters
+        current_position = list(
+            square_moved_from
+        )  # splits string into a list of characters
         destination_position = list(square_moved_to)
 
-        current_column = ord(current_position[0]) - ord('a')  # unicode conversion, a = 0
+        current_column = ord(current_position[0]) - ord(
+            "a"
+        )  # unicode conversion, a = 0
         current_row = 8 - int(current_position[1])  # reverse order of rows
 
         piece = self.get_piece_type(current_row, current_column)
 
-        destination_column = ord(destination_position[0]) - ord('a')
+        destination_column = ord(destination_position[0]) - ord("a")
         destination_row = 8 - int(destination_position[1])
 
         if self.check_if_valid_player(piece) is False:
             return False
 
-        if self.check_if_valid_atomic_move(piece, destination_column, destination_row) is False:
+        if (
+            self.check_if_valid_atomic_move(piece, destination_column, destination_row)
+            is False
+        ):
             return False
 
-        if self.check_if_valid_chess_move(piece, current_column, current_row, destination_column,
-                                          destination_row) is False:
+        if (
+            self.check_if_valid_chess_move(
+                piece, current_column, current_row, destination_column, destination_row
+            )
+            is False
+        ):
             return False
 
         if self._game_state != "UNFINISHED":
@@ -293,12 +381,30 @@ class ChessVar:
         # make indicated move
         self._board[current_row][current_column] = 0
         if self._board[destination_row][destination_column] != 0:
-            self._board[destination_row][destination_column] = 0  # capturing piece explodes
-            positions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+            self._board[destination_row][
+                destination_column
+            ] = 0  # capturing piece explodes
+            positions = [
+                (1, 0),
+                (-1, 0),
+                (0, 1),
+                (0, -1),
+                (1, 1),
+                (1, -1),
+                (-1, 1),
+                (-1, -1),
+            ]
             for row, column in positions:
                 try:
-                    if self._board[destination_row + row][destination_column + column] not in (BP, WP):  # not a pawn
-                        self._board[destination_row + row][destination_column + column] = 0
+                    if self._board[destination_row + row][
+                        destination_column + column
+                    ] not in (
+                        BP,
+                        WP,
+                    ):  # not a pawn
+                        self._board[destination_row + row][
+                            destination_column + column
+                        ] = 0
                 except IndexError:  # explosion on board edge
                     continue
         else:
@@ -344,6 +450,7 @@ class ChessVar:
         :return: int, piece at the given location"""
         return self._board[row][column]
 
+
 def validate_input(start_pos, end_pos):
     start_pos = start_pos.lower()
     end_pos = end_pos.lower()
@@ -355,7 +462,7 @@ def validate_input(start_pos, end_pos):
     if end_pos[0] not in ("a", "b", "c", "d", "e", "f", "g", "h"):
         print("Please specify a valid square to move to.")
         return False
-    
+
     # check for appropriate number
     if start_pos[1] not in (1, 2, 3, 4, 5, 6, 7, 8):
         print("Please specify a valid square to move from.")
@@ -364,10 +471,11 @@ def validate_input(start_pos, end_pos):
         print("Please specify a valid square to move to.")
         return False
 
+
 if __name__ == "__main__":
     game = ChessVar()
 
-    while True: 
+    while True:
         game.print_board()
         print("Game state:", game.get_game_state())
         print("Current player:", game.get_current_player())
